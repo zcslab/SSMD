ec1d74114fbe7d78a13297b04a0371a3f7e33956
xiaoyulu95 <42850884+xiaoyulu95@users.noreply.github.com>
2019-12-09 13:41:31 -0500

    Add files via upload

--- R/SSMD_ImmuCC.R
+++ R/SSMD_ImmuCC.R
@@ -26,7 +26,7 @@
 ImmuCC <- function(sig_matrix, mixture_file){
   
   # Load the function of CIBERSORT
-  #source("/Users/xiaoyulu/Documents/RA/NEW/201811_NMF/Pipeline/New Server/CIBERSORT_modified.R")
+  source("CIBERSORT_modified.R")
   # Note: the scirpts of CIBERSORT.R is a method developed by Newman et al.and can be accesssed upon an request from https://cibersort.stanford.edu/
   perm <- 100
   results <- CIBERSORT(sig_matrix, mixture_file, perm)
