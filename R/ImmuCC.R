aa96c2f3e6a0d90c2d18dac19cd99dfe06f79769
xiaoyulu95 <42850884+xiaoyulu95@users.noreply.github.com>
2019-12-13 11:58:53 -0500

    Add files via upload

--- R/ImmuCC.R
+++ R/ImmuCC.R
@@ -1,10 +1,16 @@
 #ImmuCC
-ImmuCC <- function(sig_matrix, mixture_file){
+.onLoad <- function(libname, pkgname) {
+  utils::data(original_sig_matrix, package = pkgname, envir = parent.env(environment()))
+  original_sig_matrix <- SSMD::original_sig_matrix
+  assign("original_sig_matrix", original_sig_matrix, envir = parent.env(environment()))
+}
+
+ImmuCC <- function(mixture_file){
   
   # Load the function of CIBERSORT
   #source("CIBERSORT_modified.R")
   # Note: the scirpts of CIBERSORT.R is a method developed by Newman et al.and can be accesssed upon an request from https://cibersort.stanford.edu/
   perm <- 100
-  results <- CIBERSORT(sig_matrix, mixture_file, perm)
+  results <- CIBERSORT(original_sig_matrix, mixture_file, perm)
   results
 }
